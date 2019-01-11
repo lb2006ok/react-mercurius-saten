c0c7c63d48c44380abb05c9d45fa20198024d2b8
LiuBo <ok_liubo@sina.com>
2019-01-11 15:30:43 +0800

    create

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "BuildControl/BuildControl.css"
+import BuildControl from 'BuildControl/BuildControl'
+
+
+const control = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'},
+]
+const buildControls = (props) => (
+    <div className="BuildControl">
+
+    </div>
+)
+
+export default buildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,18 @@ import BurgerIngredients from "./Burgeringredients/Burgeringredients";
 import './Burger.css'
 
 const Burger = props => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map(
             (_, i) => (
                 <BurgerIngredients key={igKey + i} type={igKey}></BurgerIngredients>
             )
         )
-    })
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, [])
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className="Burger">
             <BurgerIngredients type="bread-top"/>
